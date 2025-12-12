943e688a11e16bb111221be7ecf32ced0948eec8
Abdujalol <Donitursunov98@gmail.com>
2025-12-12 20:26:53 +0500

    4-mashq

--- taxi_fare_calculator.py
+++ taxi_fare_calculator.py
@@ -1,1 +1,12 @@
+from num2words import num2words
+
+dastlabki_tolov = float(input('tolov: '))
+masofa = float(input('masofa: '))
+km_uchun = 1.20
+
+umum_summa = dastlabki_tolov + masofa*km_uchun
+
+print(f'umum_summa: ${umum_summa:.2f}')
+print(num2words(umum_summa, lang='en', to='currency'))
+print(num2words(umum_summa, lang='ru', to='currency'))
 
