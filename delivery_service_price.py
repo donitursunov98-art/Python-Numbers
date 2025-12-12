bd250773533d2f684589f1e6021fac776e56ff43
Abdujalol <Donitursunov98@gmail.com>
2025-12-12 20:24:22 +0500

    5-mashq

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,1 +1,11 @@
+from num2words import num2words
 
+tolov = float(input('tolov: '))
+masofa = float(input('masofa: '))
+km_uchun = 0.80
+
+umum_summa = tolov + masofa*km_uchun
+
+print(f'umum_summa: ${umum_summa:.2f}')
+print(num2words(umum_summa, lang='en', to='currency'))
+print(num2words(umum_summa, lang='ru', to='currency'))
