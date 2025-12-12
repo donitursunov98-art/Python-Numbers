adff9af0f155a03755fb4f1d5d1cea34c1b3088d
Abdujalol <Donitursunov98@gmail.com>
2025-12-12 20:00:20 +0500

    6-mashq

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,1 +1,12 @@
+from num2words import num2words 
 
+oy_boshi = float(input('dastlabki korsatkich: '))
+oy_oxiri = float(input('oxirgi korsatgich: '))
+
+narx_1kWh = 0.45
+
+result = (oy_oxiri - oy_boshi )*narx_1kWh
+
+print(f'result: ${result:.2f}')
+print(num2words(result, lang='en'))
+print(num2words(result, lang='ru'))
